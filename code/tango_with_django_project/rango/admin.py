013bbc1426a251fdd88d0c01395dcb17f1a1a8ef
Andrew <Andrew@tegraanalytics.com>
2016-10-17 17:00:40 -0400

    Finish partway through Chapter 9

--- code/tango_with_django_project/rango/admin.py
+++ code/tango_with_django_project/rango/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
-
+from rango.models import Category, Page, UserProfile
 # Register your models here.
 
 class PageAdmin(admin.ModelAdmin):
@@ -12,3 +11,4 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
