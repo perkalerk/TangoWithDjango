080953405cd26ab4f94e74ddbcc1ff42c9cc1eae
Andrew <Andrew@tegraanalytics.com>
2016-10-21 17:03:23 -0400

    Updated up to partially through the bootstrap chapter

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from datetime import datetime
 
 def index(request):
+
+
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
@@ -11,14 +19,27 @@ def index(request):
     # that will be passed to the template engine.
 
     category_list = Category.objects.order_by('-likes')[:5]
+
     context_dict = {'categories': category_list}
 
     pages_list = Page.objects.order_by('-views')[:5]
     context_dict['pages'] = pages_list
 
-    return render(request, 'rango/index.html', context_dict)
+
+    # Call function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
+
 def about(request):
+
     context_dict = {'name': 'Andrew'}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -54,6 +75,7 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm
 
@@ -80,6 +102,7 @@ def add_category(request):
     # Render the form with error messages (if any)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -107,65 +130,153 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context_dict)
+############################# DJANGO REGISTRATION ENABLED ############################################
+# def register(request):
+#     # A boolean value for telling the template
+#     # whether the registration was successful.
+#     # Set to False initially.  Code changes value to
+#     # True when registration succeeds.
+#
+#     registered = False
+#
+#     # If it's a HTTP POST, we're interested in processing form data.
+#     if request.method == 'POST':
+#         # Attempt to grab information from the raw form information
+#         # Note that we make use of both UserForm and UserProfileForm
+#         user_form = UserForm(data=request.POST)
+#         profile_form = UserProfileForm(data=request.POST)
+#
+#         # If the two forms are valid...
+#         if user_form.is_valid() and profile_form.is_valid():
+#             # Save the user's form data to the database.
+#             user = user_form.save()
+#
+#             # Now we hash the password with the set_password method.
+#             # Once hashed, we can update the user object.
+#             user.set_password(user.password)
+#             user.save()
+#
+#             # Now sort out the UserProfile instance.
+#             # Since we need to set the user attribute ourselves,
+#             # we set commit=False.  This delays saving the model
+#             # until we're ready to avoid integrity problems.
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#
+#             # Did the user provide a profile picture?
+#             # If so, we need to get it from the input form and
+#             # put it in the UserProfile model.
+#             if 'picture' in request.FILES:
+#                 profile.picture = request.FILES['picture']
+#
+#             # Now we save the UserProfile model instance.
+#             profile.save()
+#
+#             # Update our variable to indicate that the template
+#             # registration was successful
+#
+#             registered = True
+#         else:
+#             # Invalid form or forms - mistakes or something else?
+#             # Print problems to the terminal.
+#             print(user_form.errors, profile_form.errors)
+#
+#     else:
+#         # Not a HTTP POST, so we render our form using two ModelForm instances.
+#         # These forms will be blank, ready for user input.
+#         user_form = UserForm()
+#         profile_form = UserProfileForm()
+#
+#     return render(request,
+#                   'rango/register.html',
+#                   {'user_form': user_form,
+#                   'profile_form': profile_form,
+#                   'registered': registered})
+#
+# # def user_login(request):
+# #     # If the request is a HTTP POST, try to pull out the relevant information.
+# #     if request.method == 'POST':
+# #         # Gather the username and password provided by the user.
+# #         # This information is obtained from the login form.
+# #         # We use request.POST.get('<variable>') as opposed
+# #         # to request.POST['<variable>'], because the
+# #         # request.POST.get('<variable>') returns None if the
+# #         # value does not exist, while request.POST['<variable>']
+# #         # will raise a KeyError exception.
+# #         username = request.POST.get('username')
+# #         password = request.POST.get('password')
+# #
+# #         # Use Django's machinery to attempt to see if the username/password
+# #         # combination is valid - a User object is returned if it is
+# #         user = authenticate(username=username, password=password)
+# #
+# #         # If we have a User object, the details are correct.
+# #         # If None (Python's way of representing the absence of a value), no user
+# #         # with matching credentials was found.
+# #         if user:
+# #             # Is the account active? It could have been disabled.
+# #             if user.is_active:
+# #                 # If the account is valid and active, we can log the user in.
+# #                 # We'll send the user back to the homepage.
+# #                 login(request,user)
+# #                 return HttpResponseRedirect(reverse('index'))
+# #             else:
+# #                 # An inactive account was used - no logging in!
+# #                 return HttpResponse("Your Rango account is disabled.")
+# #         else:
+# #             # Bad login details were provided.  So we can't log the user in.
+# #             try:
+# #                 user = User.objects.get(username=username)
+# #                 return HttpResponse("Invalid login details supplied for username.")
+# #             except User.DoesNotExist:
+# #                 return HttpResponse("Username does not exist")
+# #
+# #
+# #     # The request is not a HTTP POST, so display the login form.
+# #     #This scenario would most likely be a HTTP GET
+# #     else:
+# #         # No context variables to pass to the template system, hence the
+# #         # blank dictionary object...
+# #         return render(request, 'rango/login.html')
 
-def register(request):
-    # A boolean value for telling the template
-    # whether the registration was successful.
-    # Set to False initially.  Code changes value to
-    # True when registration succeeds.
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
 
-    registered = False
+# @login_required
+# def user_logout(request):
+#     # Since we know the user is logged in, we can now just log them out.
+#     logout(request)
+#     # Take the user back to the homepage.
+#     return HttpResponseRedirect(reverse('index'))
 
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information
-        # Note that we make use of both UserForm and UserProfileForm
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
-        # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
 
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves,
-            # we set commit=False.  This delays saving the model
-            # until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
 
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and
-            # put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
-            # Update our variable to indicate that the template
-            # registration was successful
-
-            registered = True
-        else:
-            # Invalid form or forms - mistakes or something else?
-            # Print problems to the terminal.
-            print(user_form.errors, profile_form.errors)
-
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        # update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
     else:
-        # Not a HTTP POST, so we render our form using two ModelForm instances.
-        # These forms will be blank, ready for user input.
-        user_form = UserForm()
-        profile_form = UserProfileForm()
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
 
-    return render(request,
-                  'rango/register.html',
-                  {'user_form': user_form,
-                  'profile_form': profile_form,
-                  'registered': registered})
+    # Update/set the visits cookie
+    request.session['visits'] = visits
--- code/tango_with_django_project/tango_with_django_project/settings.py
+++ code/tango_with_django_project/tango_with_django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -132,3 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+REGISTRATION_OPEN = True
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+REGISTRATION_AUTO_LOGIN = True
+
+LOGIN_REDIRECT_URL = '/rango/'
+
+LOGIN_URL = '/accounts/login/'
